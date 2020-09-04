20dba62851c0d2b5777f0e1453b3e88809cd2e88
Jotunbane <PawMikolajczak@gmail.com>
2020-09-04 20:28:30 +0200

    NBP Exchange Rate Downloader: Url Innit

--- src/main/java/exchanger/NbpExchangeRateDownloader.java
+++ src/main/java/exchanger/NbpExchangeRateDownloader.java
@@ -1,4 +1,20 @@
 package exchanger;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class NbpExchangeRateDownloader {
+    public void check(String currencyCode) {
+        try {
+        URL url = new URL("http://api.nbp.pl/api/exchangerates/rates/A/"+currencyCode);
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/json");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }

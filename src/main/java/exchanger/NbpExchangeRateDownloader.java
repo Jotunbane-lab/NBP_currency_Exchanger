13cd2b3c6435a1707d4df22d4a24640363a94b12
Jotunbane <PawMikolajczak@gmail.com>
2020-09-04 22:14:16 +0200

    First finished version of Exchanger package

--- src/main/java/exchanger/NbpExchangeRateDownloader.java
+++ src/main/java/exchanger/NbpExchangeRateDownloader.java
@@ -1,16 +1,31 @@
 package exchanger;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class NbpExchangeRateDownloader {
-    public void check(String currencyCode) {
+    public NbpExchangeRateResult check(String currencyCode) {
+        int responseCode = 0;
+        NbpExchangeRateSeries series = null;
+        String errorMessage = "N/A";
         try {
             URL url = new URL("http://api.nbp.pl/api/exchangerates/rates/A/" + currencyCode);
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
             conn.setRequestMethod("GET");
             conn.setRequestProperty("Accept", "application/json");
+
+            BufferedReader br = new BufferedReader(new InputStreamReader((conn.getInputStream())));
+
+
+            series = new ObjectMapper().readValue(br.readLine(), NbpExchangeRateSeries.class);
+
+            responseCode = conn.getResponseCode();
+            errorMessage = conn.getResponseMessage();
 
 
             conn.disconnect();
@@ -18,6 +33,7 @@ public class NbpExchangeRateDownloader {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return (new NbpExchangeRateResult(series, responseCode, errorMessage));
     }
 
 
--- src/main/java/exchanger/NbpExchangeRateResult.java
+++ src/main/java/exchanger/NbpExchangeRateResult.java
@@ -1,4 +1,25 @@
 package exchanger;
 
 public class NbpExchangeRateResult {
+    private NbpExchangeRateSeries series;
+    private int responseCode;
+    private String errorMessage;
+
+    public NbpExchangeRateSeries getSeries() {
+        return series;
+    }
+
+    public int getResponseCode() {
+        return responseCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public NbpExchangeRateResult(NbpExchangeRateSeries series, int responseCode, String errorMessage) {
+        this.series = series;
+        this.responseCode = responseCode;
+        this.errorMessage = errorMessage;
+    }
 }
